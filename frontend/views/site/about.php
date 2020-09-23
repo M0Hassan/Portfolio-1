c4169b29e111f9a99fea429f1f8392bf2d350362
M0Hassan <aminmohamedadan@gmail.com>
2020-09-23 20:42:20 +0300

    Second commit

---
+++ frontend/views/site/about.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>I am the 21st century programmer with knowlegde of all the cutting edge technologies available to best deliver your projects. I am highly skilled in web and mobile development. I deliver quality products to my clients. Feel free to contact me at anytime.</p>
+
+    <code><?= __FILE__ ?></code>
+</div>
---
+++ frontend/views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+use yii\helpers\Url;
+
+
+/* @var $this yii\web\View */
+
+$this->title = 'Mohamed Portfolio';
+?>
+      <h1>Mohamed Hassan</h1>
+      <h2>I'm a professional Web Developer from Nairobi Kenya</h2>
+      <a href="<?= Url::to(['site/about'])?>" class="btn-about">About Me</a>
